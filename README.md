5299bde04a2faf30562057f6ee289b1ac745bb84
zhoumingque <mail@zhoumq.cn>
2018-06-18 17:02:38 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# cli
-Polix CLI
+# Polix-cli
+[Polix](https://github.com/zhoumingque/polixjs/polix) CLI
+
+[![Build Status](https://travis-ci.org/zhoumingque/polix-cli.svg?branch=master)](https://travis-ci.org/zhoumingque/polix-cli)
+[![npm](https://img.shields.io/npm/v/npm.svg)](https://www.npmjs.com/package/polix-cli)
+[![node](https://img.shields.io/badge/node-%3E%3D8-green.svg)](https://www.npmjs.com/package/polix-cli)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://www.npmjs.com/package/polix-cli)
+
+## Install
+[![NPM](https://nodei.co/npm/polix-cli.png?compact=true)](https://nodei.co/npm/polix-cli/)
+> `npm i polix-cli -g`
+
+## Usage
+
+```bash
+$ pol -h
+
+
+  Usage: pol <command>
+
+  Options:
+
+    -v, --version  output the version number
+    -h, --help     output usage information
+
+  Commands:
+
+    init           init Polix Dev
+
+...
+```
+
+## Init LabelX dev
+
+```bash
+$ pol init [name]
+```
+
+## License
+
+MIT
---
+++ bin/pol.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const LabelX = 'https://github.com/polixjs/template.git';
+const log = require('../lib/log')({ debug: false });
+const spawn = require('hexo-util/lib/spawn');
+const pkg = require('../package.json');
+const utils = require('../lib/utils');
+const Promise = require('bluebird');
+const p = require('path');
+
+const argv = process.argv;
+if(argv.length == 1 || (argv.length == 2 && argv[1][0] != '-')){
+  console.log(`
+Usage: pol <command>
+
+Options:
+
+  -v, --version  output the version number
+  -h, --help     output usage information
+
+Commands:
+
+  init           init Polix Dev`);
+}
+
+program
+  .version(pkg.version, '-v, --version')
+  .usage('<command>');
+
+program
+  .command('init')
+  .description('init Polix Dev')
+  .action(function (args) {
+    let path = process.cwd();
+    let name;
+    name = utils.isType(utils.TYPE.String, args) ? args : 'polix-example';
+    path = p.format({
+      dir: path,
+      base: name
+    });
+    log.info(`Cloning Polix-starter to`, log.color.yellow(path));
+    spawn('git', ['clone', '--recursive', LabelX, path], {
+      stdio: 'inherit'
+    }).catch(function () {
+      log.warn('git clone failed. Copying data instead');
+      return;
+    }).then(function () {
+
+      log.info('Install dependencies');
+
+      return spawn('yarn', ['install'], {
+        cwd: path,
+        stdio: 'inherit'
+      });
+    }).then(function () {
+      log.info('Start dev with Polix!');
+    }).catch(function () {
+      log.warn('Failed to install dependencies. Please run \'npm install\' manually!');
+    });
+  });
+
+program.parse(process.argv);
